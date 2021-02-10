49224eea103ae7f478f734826ac32bc9e3484a3d
asriauliyaa <78401654+asriauliyaa@users.noreply.github.com>
2021-02-10 15:15:34 +0700

    upload file web
    upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/sttlogo.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Sistem Informasi Akademik</font> <br>
+<font Size="6">Sekolah Tinggi Teknologi Bandung</font>
+</center>
